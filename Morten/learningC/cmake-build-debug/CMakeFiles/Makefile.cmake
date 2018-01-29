98e0f5a3709cef852cb2f050295d0341fdb3a73a
Stogger <mortenmovik@gmail.com>
2018-01-29 14:16:14 +0100

    added lecture notes for learning c!

--- Morten/learningC/cmake-build-debug/CMakeFiles/Makefile.cmake
+++ Morten/learningC/cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -95,5 +95,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/cTest.dir/DependInfo.cmake"
+  "CMakeFiles/learningC.dir/DependInfo.cmake"
   )
--- Morten/learningC/cmake-build-debug/CMakeFiles/learningC.dir/DependInfo.cmake
+++ Morten/learningC/cmake-build-debug/CMakeFiles/learningC.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "/home/morten/code/git/INF237/Morten/learningC/main.c" "/home/morten/code/git/INF237/Morten/learningC/cmake-build-debug/CMakeFiles/cTest.dir/main.c.o"
+  "/home/morten/code/git/INF237/Morten/learningC/main.c" "/home/morten/code/git/INF237/Morten/learningC/cmake-build-debug/CMakeFiles/learningC.dir/main.c.o"
   )
 set(CMAKE_C_COMPILER_ID "GNU")
 
