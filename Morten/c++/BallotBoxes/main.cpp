e4542d928be2985101ab379ae863bcd003573738
Stogger <mortenmovik@gmail.com>
2018-02-05 21:06:23 +0100

    BallotBoxes ferdig!

--- Morten/c++/BallotBoxes/main.cpp
+++ Morten/c++/BallotBoxes/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
+#include <math.h>
 
 using namespace std;
 
@@ -14,7 +14,7 @@ public:
     pair<int,double> popMax();
     void printHeap();
 private:
-    pair<int,double> *a;
+    pair<int,double>* a;
     int n = 0;
     void swim(int i);
     void sink(int i);
@@ -36,18 +36,12 @@ pair<int, double> Heap::popMax() {
     return a[n+1];
 }
 void Heap::swim(int i) {
-
-    while (i > 1 && a[i/2].second < a[i].second) {
-        swap(i/2, i);
-        i = i/2;
-    }
-    /*
     int j = i/2;
-    if (j <= 1) return;
+    if (j < 1) return;
     if (a[j].second < a[i].second) {
         swap(i, j);
         swim(j);
-    }*/
+    }
 }
 void Heap::sink(int i) {
     int j = 2*i;
@@ -64,33 +58,35 @@ void Heap::swap(int i, int j) {
     a[j] = k;
 }
 void Heap::printHeap() {
+    cout << "heap of size " << n << ":" << endl;
     for (int i = 1; i <= n; ++i) {
-        cout << a[i].second << "  " << a[i].first << "\r\n";
+        cout << i << ": " << a[i].second << "  " << a[i].first << "\r\n";
     }
     cout << "******\r\n";
 }
 
 
 int main() {
-    cin >> N;
-    cin >> B;
-    int pop; //population in village
-    Heap heap = *new Heap(N);
-    for (int i = 0; i < N; ++i) {
-        cin >> pop;
-        (heap).push(*new pair<int, double>(pop, 1));
+    while (true) {
+        cin >> N;
+        cin >> B;
+        if (N < 0) break;
+        int pop; //population in village
+        Heap heap = *new Heap(N);
+        for (int i = 0; i < N; ++i) {
+            cin >> pop;
+            heap.push(*new pair<int, double>(1, pop));
+        }
+        for (int j = N + 1; j <= B; ++j) {
+            pair<int, double> curMax = heap.popMax();
+            int boxes = curMax.first + 1;
+            double ppl = curMax.second * curMax.first / (double) boxes;
+            pair<int, double> newMax = *new pair<int, double>(boxes, ppl);
+            heap.push(newMax);
+        }
+        cin;
+        cout << (int) ceil(heap.popMax().second) << endl;
     }
-    for (int j = N +1; j <= B; ++j) {
-        heap.printHeap();
-        pair<int, double> curMax = heap.popMax();
-        pair<int, double> newMax = *new pair<int, double>
-                (curMax.first +1, curMax.second * curMax.first /(double) (curMax.first +1));
-        heap.push(newMax);
-    }
-    heap.printHeap();
-
-    cout << heap.popMax().first;
-
     return 0;
 }
 //vector<vector<int>> a (N, vector<int>(B)); //can initialize vvi this way! :P
