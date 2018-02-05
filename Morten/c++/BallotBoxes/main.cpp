a2ded5c39cfe2e4ced8971f99be17ea4b62c61ba
Stogger <mortenmovik@gmail.com>
2018-02-05 20:14:11 +0100

    debugger BallotBoxes

--- Morten/c++/BallotBoxes/main.cpp
+++ Morten/c++/BallotBoxes/main.cpp
@@ -1,39 +1,96 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
 
 using namespace std;
 
 int N; //number of villages
 int B; //number of ballot boxes
-int *a;
 
-void constructor(int *a) {
-    a = (int*) malloc(N*sizeof(int));
-    a[0] = 1;
-    N= 20;
+class Heap {
+public:
+    Heap(int N);
+    void push(pair<int, double> p);
+    pair<int,double> popMax();
+    void printHeap();
+private:
+    pair<int,double> *a;
+    int n = 0;
+    void swim(int i);
+    void sink(int i);
+    void swap(int i, int j);
+};
+
+Heap::Heap(int N) {
+    a = (pair<int,double>*) malloc((N+1)*sizeof(pair<int,double>));
+}
+void Heap::push(pair<int, double> p) {
+    n++;
+    a[n] = p;
+    swim(n);
+}
+pair<int, double> Heap::popMax() {
+    swap(1, n);
+    n--;
+    sink(1);
+    return a[n+1];
+}
+void Heap::swim(int i) {
+
+    while (i > 1 && a[i/2].second < a[i].second) {
+        swap(i/2, i);
+        i = i/2;
+    }
+    /*
+    int j = i/2;
+    if (j <= 1) return;
+    if (a[j].second < a[i].second) {
+        swap(i, j);
+        swim(j);
+    }*/
+}
+void Heap::sink(int i) {
+    int j = 2*i;
+    if (j > n) return;
+    if (j < n && a[j].second < a[j+1].second) j++;
+    if (a[i].second < a[j].second) {
+        swap(i, j);
+        sink(j);
+    }
+}
+void Heap::swap(int i, int j) {
+    pair<int, double> k = a[i];
+    a[i] = a[j];
+    a[j] = k;
+}
+void Heap::printHeap() {
+    for (int i = 1; i <= n; ++i) {
+        cout << a[i].second << "  " << a[i].first << "\r\n";
+    }
+    cout << "******\r\n";
 }
 
-void push(int a) {
-    //put at last index:
-
-}
-
-void pop(int a) {
-
-}
 
 int main() {
-    constructor(a);
-    //cout << a[0];
-
-    //vector<vector<int>> a (N, vector<int>(B)); can initialize vvi this way! :P
-
-
+    cin >> N;
+    cin >> B;
+    int pop; //population in village
+    Heap heap = *new Heap(N);
     for (int i = 0; i < N; ++i) {
-        int a;
-        //cin >> a;
+        cin >> pop;
+        (heap).push(*new pair<int, double>(pop, 1));
     }
+    for (int j = N +1; j <= B; ++j) {
+        heap.printHeap();
+        pair<int, double> curMax = heap.popMax();
+        pair<int, double> newMax = *new pair<int, double>
+                (curMax.first +1, curMax.second * curMax.first /(double) (curMax.first +1));
+        heap.push(newMax);
+    }
+    heap.printHeap();
 
+    cout << heap.popMax().first;
 
     return 0;
 }
+//vector<vector<int>> a (N, vector<int>(B)); //can initialize vvi this way! :P
--- Morten/java/INF237Set3/src/NestedDolls.java
+++ Morten/java/INF237Set3/src/NestedDolls.java
@@ -3,6 +3,7 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
 
 public class NestedDolls {
@@ -14,62 +15,48 @@ public class NestedDolls {
 		for (int t = 0; t < T; t++) {
 			int m = in.nextInt(); //#of dolls
 			Doll[] wDolls = new Doll[m]; //sorted by width
-			Doll[] hDolls = new Doll[m]; //sorted by height
-			
+
 			//sorting lists:
 			Arrays.sort(wDolls, new Comparator<Doll>() {
 				@Override
-				public int compare(Doll o1, Doll o2) { return o1.w - o2.w; }
+				public int compare(Doll o1, Doll o2) { return -o1.w + o2.w; }
 				});
-			Arrays.sort(hDolls, new Comparator<Doll>() {
 
-				@Override
-				public int compare(Doll o1, Doll o2) { return o1.h - o2.h; }
-			});
-			
-			ArrayList<Integer> progress = new ArrayList<Integer>();
+			ArrayList<Doll> stacks = new ArrayList<Doll>(); //heap?
+			int progressionIndex = 0;
 			
 			for (int i = 0; i < m; i++) { wDolls[i].id = i; }
-			for (int j = 0; j < m; j++) { hDolls[j].ref = j; }
-			
+
 			int count = 0; //number of stacks of dolls
-			int i = 0, j = 0; //i is position in wDolls, j is corresponding pos in hDolls
 			Doll prev = wDolls[0];
-			while(true) {
-				Doll doll = wDolls[i++];
+			for (int i = 0; i < m; i++) {
+				Doll doll = wDolls[i];
 				
-				if (doll.w <= prev.w) { 
+				if (doll.w == prev.w) { 
 					count++; 
-					continue; 
+					prev = doll;
+					continue; //no need to remember this stack, cannot continue it
 				}
-				if (doll.h <= prev.h) {
-					count++;
-					for (int l : progress) {
-						if (wDolls[l].w > doll.w && wDolls[l].h > doll.h) {
-							continue; //keep going, no need to 
+				if (doll.h <= prev.h) { //need new stack or to continue an existing
+					for (int k = 0; k < stacks.size(); k++) { //binary search?
+						if (stacks.get(k).w < doll.w && stacks.get(k).h < doll.h) {
+							
+							
+							continue;
 						}
 					}
-					progress.add(i);
-					
+					count++;
+//					stacks.add(i); //remember where we left off, so we might continue this one later.
 				}
-				
-				
 			}
-			
 		}
-		
-		
+	}
+	
+	private class BST {
 		
 	}
 	
-	public class Pair {
-		int i, j;
-		public Pair(int i, int j) {
-			this.i = i;
-			this.j = j;
-		}
-	}
-
+	
 	public class Doll {
 		int w, h;
 		int id, ref;
